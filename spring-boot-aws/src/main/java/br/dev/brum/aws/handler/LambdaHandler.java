e8e6e456afa820542058da425fcfaf0f61175a94
lucasbrum <brum.lucas@icloud.com>
2023-08-07 07:32:36 -0300

    Adding aws sam in gitignore

--- spring-boot-aws/src/main/java/br/dev/brum/aws/handler/LambdaHandler.java
+++ spring-boot-aws/src/main/java/br/dev/brum/aws/handler/LambdaHandler.java
@@ -1,28 +1,30 @@
 package br.dev.brum.aws.handler;
 
+import br.dev.brum.aws.SpringBootAwsApplication;
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
-import javax.ws.rs.core.Application;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
     static {
         try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
-        } catch (ContainerInitializationException ex) {
-            throw new RuntimeException("Unable to load spring boot application", ex);
-        }
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(SpringBootAwsApplication.class); }
+        catch (ContainerInitializationException ex){
+            throw new RuntimeException("Unable to load spring boot application",ex); }
     }
 
     @Override
-    public AwsProxyResponse handleRequest(AwsProxyRequest awsProxyRequest, Context context) {
-
-        return handler.proxy(awsProxyRequest, context);
+    public AwsProxyResponse handleRequest(AwsProxyRequest input, Context context) {
+        return handler.proxy(input, context);
     }
 }
