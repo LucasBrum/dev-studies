ea4c7b5ec3ccbc0eb706d6062171266ef11a4f58
Lucas Brum <finamorebrum@gmail.com>
2023-09-06 08:15:00 -0300

    Adding javascript and typescript studies

---
+++ JavaScript & TypeScript/JavaScript Básico/aula3/index.js
@@ -0,0 +1,7 @@
+console.log('Lucas Brum');
+console.log("Lucas 'Brum'");
+console.log(`'Lucas Brum'`);
+console.log(123456, 15.05, 'Lucas Brum'); // Tudo é number no Javascript
+
+console.log('Meu nome é "Lucas Brum". Estou aprendendo JavaScript às', 8, 'da manhã.')
+
